17c4942729841cedb24f3332beeafff614119d6f
joeTid09 <joseph.tidwell09@gmail.com>
2024-01-21 15:53:23 -0600

    updated with plant pictures and a more fitting background

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,7 +6,7 @@ import { HousingLocation } from '../housing-location';
   imports: [],
   template: `
   <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <img class="listing-photo" src="{{housingLocation.photo}}" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
   </section>
