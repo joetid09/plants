57060c0c13d92ccdad588850d0ddf80549b34021
joeTid09 <joseph.tidwell09@gmail.com>
2024-01-21 12:09:47 -0600

    writing over

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,
+    HomeComponent],
+  // templateUrl: './app.component.html',
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/monstera.png" alt="logo" style='width: 50px; 
+        height: 50px;' aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
