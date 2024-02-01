ce25adc0cbde3dd169c9281972582bcf7d5acae5
joeTid09 <joseph.tidwell09@gmail.com>
2024-01-31 21:13:03 -0600

    updated login to fetch api

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -6,8 +6,15 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
   constructor() { }
+  readonly url = 'http://localhost:5123/Account'
+  // submitLogin(username: string, password: string) {
+  //   console.log(`Login received: username: ${username}, password: ${password}.`);
+  // }
 
-  submitLogin(username: string, password: string) {
-    console.log(`Login received: username: ${username}, password: ${password}.`);
+  async submitLogin(username: string, password: string): Promise<boolean> {
+    const data = await fetch(`${this.url}/${username}`);
+    const results = await data.json() ?? {};
+    console.log("heres the results" + results);
+    return results;
   }
 }
